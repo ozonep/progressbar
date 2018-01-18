b9aad5ce6c0c0d72e1466e91700da90bfb6d162e
Ivan Malkov <ozonep@yandex.ru>
2018-01-18 23:27:21 +0300

    Further JS optimizations

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,19 @@
-let percent = document.querySelector("#percent");
-let circle = document.querySelector("#svg #bar");
-let cont = document.querySelector("#cont");
+const percent = document.querySelector("#percent");
+const circle = document.querySelector("#svg #bar");
+const cont = document.querySelector("#cont");
 
 function animate() {
     let val = parseInt(percent.value);
-    if (isNaN(val)) {
+    if (isNaN(val) || val > 100) {
         val = 100;
-    } else {
-        let r = circle.getAttribute("r");
-        let c = Math.PI*(r*2);
-        if (val < 0) {
-            val = 0;
-        } else if (val > 100) {
-            val = 100;
-        }
-        let pct = ((100-val)/100)*c;
-        circle.style.strokeDashoffset = pct.toString();
-        cont.setAttribute("data-pct", val.toString());
+    } else if (val < 0) {
+        val = 0;
     }
+    let r = circle.getAttribute("r");
+    let c = Math.PI*(r*2);
+    let pct = ((100-val)/100)*c;
+    circle.style.strokeDashoffset = pct.toString();
+    cont.setAttribute("data-pct", val.toString());
 }
 
 percent.addEventListener("input", animate, false);
